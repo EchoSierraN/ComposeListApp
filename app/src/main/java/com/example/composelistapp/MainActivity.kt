ae4b4c9961c3192a653f1ebc60ddb5e6012b6b4b
Elias Shemsu <echosierran@gmail.com>
2024-03-22 11:16:59 +0300

    Testing jetpack compose state

--- app/src/main/java/com/example/composelistapp/MainActivity.kt
+++ app/src/main/java/com/example/composelistapp/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.composelistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composelistapp.ui.theme.ComposeListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +33,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeListAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                var count by remember {
+                    mutableStateOf(0)
+                }
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    Text(text = count.toString(), fontSize = 20.sp)
+                    Button(onClick = { count++ }) {
+                        Text(text = "Click Me!")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeListAppTheme {
-        Greeting("Android")
     }
 }
