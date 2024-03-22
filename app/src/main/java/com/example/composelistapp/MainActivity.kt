10c9ed564723c059338177a9b33d95c8562968d6
Elias Shemsu <echosierran@gmail.com>
2024-03-22 12:21:55 +0300

    Name list

--- app/src/main/java/com/example/composelistapp/MainActivity.kt
+++ app/src/main/java/com/example/composelistapp/MainActivity.kt
@@ -6,12 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,21 +38,55 @@ import androidx.compose.ui.unit.sp
 import com.example.composelistapp.ui.theme.ComposeListAppTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeListAppTheme {
-                var count by remember {
-                    mutableStateOf(0)
+                var name by remember {
+                    mutableStateOf("")
                 }
+                var names by remember {
+                    mutableStateOf(listOf<String>())
+                }
+
                 Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
                 ) {
-                    Text(text = count.toString(), fontSize = 20.sp)
-                    Button(onClick = { count++ }) {
-                        Text(text = "Click Me!")
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        OutlinedTextField(
+                            modifier = Modifier.weight(6f),
+                            value = name,
+                            onValueChange = { text ->
+                                name = text
+                            })
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(
+                            modifier = Modifier.weight(2.5f),
+                            onClick = {
+                            if(name.isNotEmpty()) {
+                                names = names + name
+                                name = ""
+                            }
+                        }) {
+                            Text(text = "Add")
+                        }
+                    }
+                    LazyColumn {
+                        items(names.reversed()) { currentName ->
+                            Text(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp),
+                                text = currentName,
+                            )
+                            Divider()
+                        }
                     }
                 }
             }
